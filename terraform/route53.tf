89d1ac77c548400fb200e5031328136f40e7e006
jsalinas4 <jsalinasr3@upao.edu.pe>
2025-11-20 04:38:55 -0500

    feat: agregar alb certificado

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -39,3 +39,35 @@ resource "aws_acm_certificate_validation" "cloudfront_cert" {
   certificate_arn         = aws_acm_certificate.cloudfront_cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]  # Changed from fqld to fqdn
 }
+
+resource "aws_acm_certificate" "alb_cert" {
+  domain_name               = "alb.${var.domain_name}"
+  validation_method         = "DNS"
+  subject_alternative_names = ["www.alb.${var.domain_name}"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "alb_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.alb_cert.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "alb_cert_validation" {
+  certificate_arn         = aws_acm_certificate.alb_cert.arn
+  validation_record_fqdns = [for rec in aws_route53_record.alb_cert_validation : rec.fqdn]
+}
