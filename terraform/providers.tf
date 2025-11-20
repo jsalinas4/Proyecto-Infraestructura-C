7c614325c4be6d5d424fec463682e4a394cc6553
jsalinas4 <jsalinasr3@upao.edu.pe>
2025-11-20 01:39:08 -0500

    feat: agregar efs y actualizar providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,25 @@
 provider "aws" {
-  region = "us-east-2"
-  profile = ""
+  region  = var.region  # This will be us-east-2
+  profile = "joseph"
+  allowed_account_ids = ["879381246410"]
+  default_tags {
+    tags = {
+      environment = var.env
+      managedby   = "terraform"
+    }
+  }
+}
+
+# Add provider configuration for us-east-1 (required for CloudFront certificates)
+provider "aws" {
+  alias   = "us-east-1"
+  region  = "us-east-1"
+  profile = "joseph"
+  allowed_account_ids = ["879381246410"]
+  default_tags {
+    tags = {
+      environment = var.env
+      managedby   = "terraform"
+    }
+  }
 }
