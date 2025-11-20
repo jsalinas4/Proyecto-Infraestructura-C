d78b254594f85bd9526828ec3359a3bc90986342
jsalinas4 <jsalinasr3@upao.edu.pe>
2025-11-20 04:45:01 -0500

    style: renombrar carpeta template y eliminar lineas vacias

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -106,34 +106,16 @@ resource "aws_lb_target_group" "app_tg" {
   }
 }
 
-resource "aws_lb_listener" "front_end" {
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app_alb.arn
   port              = 80
   protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.app_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.cloudfront_cert.arn
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
-
 
 resource "aws_security_group" "alb_sg" {
   name        = "${var.env}-alb-sg"
